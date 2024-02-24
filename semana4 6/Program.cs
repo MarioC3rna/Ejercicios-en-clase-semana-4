eaabc999d63e0ea34007e2492731bb82ee1ca6fc
MARIO FERNANDO CERNA NAJERA <mcernan@miumg.edu.gt>
2024-02-24 11:15:26 -0600

    Agregar archivos de proyecto.

---
+++ semana4 6/Program.cs
@@ -0,0 +1,20 @@
+﻿//Console.WriteLine("NUMEROS PARES");
+//int x = 2;
+//while (x <= 10)
+//{
+//    Console.WriteLine(x);
+//    x += 2;
+
+//}
+
+
+
+//Console.WriteLine("Suma de numeros naturales");
+//int re = 0;
+//for (int x= 1; x <=10; x++) 
+//{
+//   re +=x;
+//}
+
+//Console.WriteLine($"La suma es :" + re);
+
